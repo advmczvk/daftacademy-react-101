61ef97fe1a9167f4f28d7ddc78e186aeeb619503
advmczvk <kuba.adamczyk99@gmail.com>
2022-05-16 15:20:27 +0200

    feat: :sparkles: added alert handler and link to github repo
    Added handleShowAlert function to App.tsx, styled the button and added clickable image with link to github repo

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const handleShowAlert = () => {
+    alert("Hello react!");
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Daftacademy F4B 101
         </p>
+        <button className="alert-button" onClick={handleShowAlert}>Hello</button>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
+        <a href="https://github.com/advmczvk/daftacademy-react-101"><img src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg" alt="GitHub logo" /></a>
       </header>
     </div>
   );
